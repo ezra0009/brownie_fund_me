480344f090c66288b19cd9376fa222fdf37b3000
unknown <ezra009@gmail.com>
2022-08-28 17:00:31 +0600

    first commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,60 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+contract FundMe {
+    using SafeMathChainlink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address public owner;
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        uint256 minimumUSD = 5 * 10**18;
+        require(
+            getConversionRate(msg.value) >= minimumUSD,
+            "you need to spend more eth"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function getVersion() public view returns (uint256) {
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 10**10);
+    }
+
+    //1886.33000000 * 10 ** 8
+
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethAmount * ethPrice) / 10**18;
+        return ethAmountInUsd;
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        uint256 minimumUSD = 5 * 10**18;
+        uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        return (minimumUSD * precision) / price;
+    }
+
+    function withdraw() public payable {
+        require(msg.sender == owner);
+        msg.sender.transfer(address(this).balance);
+    }
+}
